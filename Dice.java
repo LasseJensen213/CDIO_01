4829994b93c25b275a9609e984feeddbb5be6b75
LasseJensen213 <lassejensen213@gmail.com>
2016-10-03 19:14:35 +0200

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,35 @@
+package dice;
+
+public class Dice {
+
+	private int faceValue;
+
+	// Roll the dices	
+	public void roll() {
+		faceValue = (int)(Math.random()*6+1);
+	}
+
+	//Compare faceValue
+	public boolean isEqual(Dice d) {
+		if(d.getFaceValue()==this.faceValue){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+
+
+	//Getters and settes
+	public int getFaceValue() {
+		return faceValue;
+	}
+
+	public void setFaceValue(int faceValue) {
+		this.faceValue = faceValue;
+	}
+
+
+}
